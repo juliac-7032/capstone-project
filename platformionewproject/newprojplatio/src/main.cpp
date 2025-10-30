af1241c10643ed3662e582530f515e45b33c33b4
juliac-7032 <juliacawkwell3@gmail.com>
2025-10-29 20:04:27 -0600

    made yellow output

--- platformionewproject/newprojplatio/src/main.cpp
+++ platformionewproject/newprojplatio/src/main.cpp
@@ -2,27 +2,31 @@
 #include <Adafruit_NeoPixel.h>
 
 
-#define PIN 6
+#define RED 6
+#define GREEN 7
 
+double Tms = 1.0; 
+double duty = 0.1;
 
 // put function declarations here:
 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PIN, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
   Serial.begin(9600);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- digitalWrite(PIN, HIGH);
- Serial.println("Pin is high");
- delay(10000);
- digitalWrite(PIN, LOW);
- Serial.println("Pin is low");
- delay(10000);
-
+ digitalWrite(RED, HIGH);
+ digitalWrite(GREEN, HIGH);
+ delay(Tms*duty);
+ digitalWrite(RED, LOW);
+ digitalWrite(GREEN, HIGH);
+ delay(1.0 - (Tms*duty));
 }
 
 // put function definitions here:
