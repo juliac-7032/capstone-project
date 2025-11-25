9996fc5685a6087084be0004376e68921217075b
juliac-7032 <juliacawkwell3@gmail.com>
2025-11-24 21:13:47 -0700

    light turns red on motion detection

--- platformionewproject/newprojplatio/src/main.cpp
+++ platformionewproject/newprojplatio/src/main.cpp
@@ -4,46 +4,37 @@
 #include <chrono> 
 #include <ArduinoJson.h>
 
-int red =  6;
+int red =  6; //name of pin on ESP32
 int green = 7;
+int PIR = 2;
+int PIR_val = 0;
 int delay_time = 500;
 double Tms = 1.0; 
 double duty = 0.1;
-
-enum State{unoccupied = 0, occupied= 1, maintenance =2};
 
 void setup() {
   
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
+  pinMode(PIR, INPUT);
   Serial.begin(9600);
   
 }
 
 void loop() {
-
-  const char* input = "{\"sensor state\": \"unoccupied\"}"; //json input
-  JsonDocument doc; //craete document to hold the json
-  deserializeJson(doc, input); //put the input into the JsonDocument
   
   //State current_state = doc["sensor state"]; //json will map to the state variable
-
-  State current_state = occupied;
-
-  switch(current_state){
-    case unoccupied: green_light(Tms, duty, green);
-                    Serial.println("green");
-                    break;
-    case occupied: red_light(Tms, duty, red);
-                    Serial.println("red");
-                    break;
-    case maintenance: yellow_light(Tms, duty, red, green); 
-                      Serial.println("yellow");                                
-                      break;
-    default: green_light(Tms, duty, green); 
-             Serial.println("default");                           
-
+  
+  PIR_val = digitalRead(PIR);
+  if(PIR_val == HIGH){ //high on pir pin means motion detected
+    red_light(Tms, duty, red);
   }
+
+  else{
+    green_light(Tms, duty, green);
+  }
+
+  
   
   
 }
