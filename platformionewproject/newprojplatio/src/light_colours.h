fb75b0ac52825fcb256ebb8b0537baeece3aec2d
juliac-7032 <juliacawkwell3@gmail.com>
2026-01-22 20:59:48 -0700

    added code for sensors

---
+++ platformionewproject/newprojplatio/src/light_colours.h
@@ -0,0 +1,13 @@
+#ifndef LIGHT_COLOURS.H
+#define LIGHT_COLOURS.H
+
+void yellow_light(double Tms, double duty, int red, int green);
+
+void green_light(double Tms, double duty, int green);
+
+void red_light(double Tms, double duty, int red);
+
+
+//TODO data structs for the state data to pass into the enter functions
+
+#endif
