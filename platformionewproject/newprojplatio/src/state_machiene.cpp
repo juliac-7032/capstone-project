27261d72d555c8219d191969233b6da6d80b22a4
juliac-7032 <juliacawkwell3@gmail.com>
2026-02-25 14:02:01 -0700

    changed duty cycle

--- platformionewproject/newprojplatio/src/state_machiene.cpp
+++ platformionewproject/newprojplatio/src/state_machiene.cpp
@@ -11,7 +11,7 @@ static const int g_pwmRes  = 8;    // 8-bit => 0..255
 static const int g_chRed   = 0;
 static const int g_chGreen = 1;
 
-static uint8_t g_dutyOn = 255;
+static uint8_t g_dutyOn = 100;
 
 static void pwm_write(int channel, uint8_t duty) {
   ledcWrite(channel, duty);
